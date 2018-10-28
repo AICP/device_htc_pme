7295b9a73bac8ccbf27658a797ed0c189a1c9b49
fagyi <fagyipda@gmail.com>
2018-10-28 11:46:31 +0100

    pme: remove powerhint

--- device.mk
+++ device.mk
@@ -379,9 +379,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.power@1.1-service-qti
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
-
 # Prebuilts
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/prebuilt/org.apache.http.legacy.jar:$(TARGET_COPY_OUT_SYSTEM)/framework/org.apache.http.legacy.jar
