ece0c9eaebb447149ab1fd4d4deaf92af59639bd
fagyi <fagyipda@gmail.com>
2019-01-29 19:13:22 +0100

    pme: add doze package for AOD
    First bringup.
    
    This works, how I like it. Lift to check, hand wave, and pocket is there. Otherwise only enabled when notification coming.
    
    Otherwise its disabled for battery saving.
    
    The best if its disabled of course, just reduced the drain with this.

--- device.mk
+++ device.mk
@@ -206,6 +206,10 @@ PRODUCT_PACKAGES += \
     libqdMetaData.system \
     libtinyxml
 
+# Doze
+PRODUCT_PACKAGES += \
+    HTCDoze
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
