e2e5eec6569a5d21824f0d1259665081c13da75e
Ali B <abittin@gmail.com>
2019-02-09 23:21:41 +0300

    pme: correct vendor security patch level
    Change-Id: I361a0e6db9ee4377681ced4ad75d7d0caee51ce9

--- device.mk
+++ device.mk
@@ -466,7 +466,7 @@ PRODUCT_COPY_FILES += \
 
 # Vendor security patch level
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.aicp.build.vendor_security_patch=2018-12-01
+    ro.aicp.build.vendor_security_patch=2017-12-01
 
 # Vibrator
 PRODUCT_PACKAGES += \
