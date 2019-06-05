b0f3348de77c80d19d7792804015b9fce1a6e8e7
Ali B <abittin@gmail.com>
2019-06-05 19:40:41 +0300

    pme: Migrate to LiveDisplay 2.0
    Change-Id: I68c67f9615e81673d47f432c42b1f276848121e3

--- device.mk
+++ device.mk
@@ -318,7 +318,7 @@ PRODUCT_PACKAGES += \
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@1.0-service-sdm
+    vendor.lineage.livedisplay@2.0-service-sdm
 
 # Marlin IRQ Balancer configuration file
 PRODUCT_COPY_FILES += \
